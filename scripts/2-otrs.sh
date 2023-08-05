2d23d001777f2aab879080891f50ed0fe052d94a
Mantenedor <wagtonazevedo@gmail.com>
2023-08-05 20:56:03 -0300

    znuny-latest-6.5.tar.gz	2023-06-14 08:18

--- scripts/2-otrs.sh
+++ scripts/2-otrs.sh
@@ -4,7 +4,7 @@ echo "Instalando OTRS..."
 
 apt-get install -y default-mysql-client libapache2-mod-perl2 libdbd-mysql-perl libtimedate-perl libnet-dns-perl libnet-ldap-perl libio-socket-ssl-perl libpdf-api2-perl libdbd-mysql-perl libsoap-lite-perl libtext-csv-xs-perl libjson-xs-perl libapache-dbi-perl libxml-libxml-perl libxml-libxslt-perl libyaml-perl libarchive-zip-perl libcrypt-eksblowfish-perl libencode-hanextra-perl libmail-imapclient-perl libtemplate-perl libmoo-perl libjavascript-minifier-xs-perl libdatetime-perl libcss-minifier-xs-perl libauthen-ntlm-perl libdbd-odbc-perl
 
-cd /opt/ && time wget https://download.znuny.org/releases/znuny-latest-6.0.tar.gz && tar -xzvf znuny-latest-6.0.tar.gz && ln -s znuny-6.0.46/ otrs
+cd /opt/ && time wget https://download.znuny.org/releases/znuny-6.5.3.tar.gz && tar -xzvf znuny-6.5.3.tar.gz && ln -s znuny-6.5.3/ otrs
 
 perl /opt/otrs/bin/otrs.CheckModules.pl && perl -MCPAN -e shell;
 
